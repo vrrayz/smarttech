37076a53c9a294939d06c3a3de3da0c4a873e826
Victor <emmyvic98@gmail.com>
2023-11-12 09:22:02 +0100

    navbar mobile

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import React, { useState } from "react";
+import styled from "styled-components";
+
+export const Navbar = () => {
+  const [isNavToggled, setIsNavToggled] = useState(false);
+  return (
+    <NavbarContainer>
+      <h1 className="logo">SmartTech</h1>
+      <ToggleMenuContainer>
+        <a className="text-small btn btn-primary nav-button">Get a quote</a>
+        <MobileNavToggler
+          className={`${isNavToggled && "transformed"}`}
+          onClick={() => setIsNavToggled(!isNavToggled)}
+        >
+          <TogglerItem
+            className={`${isNavToggled && "transformed"}`}
+          ></TogglerItem>
+          <TogglerItem
+            className={`${isNavToggled && "transformed"}`}
+          ></TogglerItem>
+          <TogglerItem
+            className={`${isNavToggled && "transformed"}`}
+          ></TogglerItem>
+        </MobileNavToggler>
+      </ToggleMenuContainer>
+    </NavbarContainer>
+  );
+};
+
+const NavbarContainer = styled.div`
+  padding: 8px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  box-shadow: 0px 0px 8px;
+
+  .logo {
+    font-family: "Poppins", sans-serif;
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+`;
+const ToggleMenuContainer = styled.div`
+display: flex;
+justify-content: space-around;
+
+a.nav-button{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+`;
+const MobileNavToggler = styled.div`
+  width: 40px;
+  height: 40px;
+  margin: auto 0px;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+  cursor: pointer;
+  transition: padding 200ms;
+  z-index: 100;
+  &.transformed {
+    padding: 8px 0px;
+  }
+`;
+const TogglerItem = styled.div`
+  width: 26px;
+  height: 3px;
+  background: #1a1221;
+  box-shadow: 1px 0px 3px #663399;
+  margin: auto;
+  border-radius: 25%;
+  transition: margin 200ms, transform 200ms;
+  &:nth-child(3) {
+    width: 13px;
+    margin-right: 7px;
+    margin-bottom: 9px;
+  }
+  &:nth-child(1) {
+    width: 13px;
+    margin-left: 7px;
+    margin-top: 9px;
+  }
+  &.transformed:nth-child(3) {
+    margin-right: 9px;
+    margin-bottom: 5px;
+    transform: rotate(45deg);
+  }
+  &.transformed:nth-child(1) {
+    margin-left: 9px;
+    margin-top: 5px;
+    transform: rotate(45deg);
+  }
+  &.transformed:nth-child(2) {
+    transform: rotate(-45deg);
+  }
+`;
