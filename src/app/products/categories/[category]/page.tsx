23e790bbd9d6c57826c02d4b9795d21a1bb9995c
Victor <emmyvic98@gmail.com>
2023-12-24 09:28:09 +0100

    smart home products added

--- src/app/products/categories/[category]/page.tsx
+++ src/app/products/categories/[category]/page.tsx
@@ -4,9 +4,18 @@ import { products } from "@/data/products";
 import React, { useMemo } from "react";
 import Image from "next/image";
 import styled from "styled-components";
+import { Home } from "@mui/icons-material";
 import { SCREENS } from "@/screens";
+import { Card } from "@/components/Card";
+import parse from 'html-react-parser';
 
-export type Category = "smart_home_automation_lighting";
+export type Category =
+  | "smart_home_automation_lighting"
+  | "door_bells_and_access_control"
+  | "alexa_voice_assistant"
+  | "smart_locks"
+  | "smart_detectors"
+  | "camera_list_and_specifications";
 
 const Page = ({ params }: { params: { category: Category } }) => {
   const currentCategory = useMemo(
@@ -17,6 +26,11 @@ const Page = ({ params }: { params: { category: Category } }) => {
     () => params.category.toUpperCase().split("_").join(" "),
     [params.category]
   );
+  const categoriesList = useMemo(() => {
+    return Object.keys(products).map((element) =>
+      element.toUpperCase().split("_").join(" ")
+    );
+  }, []);
   return (
     <main>
       <Section header={""} className="mt-8 md:mt-16">
@@ -30,10 +44,97 @@ const Page = ({ params }: { params: { category: Category } }) => {
           <SectionHeader className="md:my-3">{categoryTitle}</SectionHeader>
         </CategoryPlaceholder>
       </Section>
+      <Section header="">
+        <BreadCrumbs>
+          <li className="flex">
+            <Home fontSize="small" />
+          </li>
+          <li>Products</li>
+          <li className="active">{categoryTitle}</li>
+        </BreadCrumbs>
+      </Section>
+      <Section header="">
+        <div className="flex flex-col-reverse md:flex-row gap-8">
+          <Card style={{maxHeight: '400px'}}>
+            <CategoriesList>
+              {categoriesList.map((category, i) => (
+                <CategoryItem key={i}>
+                  <a href={`/products/categories/${category}`}>{category}</a>
+                </CategoryItem>
+              ))}
+            </CategoriesList>
+          </Card>
+          <ProductsList className="grow">
+            {currentCategory.items.map(({title, image, description}, i) => (
+              <Card key={i}>
+                <div className="flex flex-col lg:flex-row-reverse">
+                  <Image src={image} alt={title} width={300} height={300} />
+                  <div className="card-body mx-auto">
+                    <h4 className="card-heading">{title}</h4>
+                    <p>{parse(description)}</p>
+                  </div>
+                </div>
+              </Card>
+            ))}
+          </ProductsList>
+        </div>
+      </Section>
     </main>
   );
 };
+const ProductsList = styled.div`
+  display: grid;
+  grid-template-rows: 1fr;
+  gap: 16px;
+  width: 100%;
 
+  ${Card} img{
+    width: 100%;
+    height: 100%;
+    max-width: 200px;
+    max-height: 200px;
+    margin: auto;
+    object-fit: contain;
+  }
+  ${Card} .card-body{
+    padding: 16px;
+  }
+  ${Card} .card-heading{
+    padding: 0px;
+    font-size: 1.2rem;
+    font-weight: 600;
+    margin-bottom: 12px;
+    text-align:center;
+  }
+  ${Card} .card-body p{
+    font-weight: 200;
+    font-size: 0.8rem;
+    color: #000;
+  }
+  ${SCREENS.lg}{
+    width: 60%;
+    ${Card} .card-heading{
+      text-align:left;
+    }
+  }
+`;
+const CategoriesList = styled.ul`
+  font-weight: 200;
+  padding: 12px 0px;
+  font-size: 0.8rem;
+`;
+const CategoryItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  a {
+    padding: 12px;
+    width: 100%;
+  }
+  a:hover {
+    background-color: #051f80;
+    color: #fff;
+  }
+`;
 const CategoryPlaceholder = styled.div`
   ${SectionHeader} {
     font-size: 18px;
@@ -49,6 +150,27 @@ const CategoryPlaceholderImage = styled(Image)`
 
   ${SCREENS.md} {
     height: 230px;
+  }
+`;
+
+const BreadCrumbs = styled.ul`
+  font-size: 12px;
+  display: flex;
+  gap: 1px;
+  border-bottom: 1px dotted;
+  padding-bottom: 6px;
+
+  li::after {
+    content: ">";
+    margin-left: 4px;
+    margin-right: 4px;
+  }
+  li.active::after {
+    content: "";
+  }
+  li.active {
+    color: #284f67;
+    font-weight: bold;
   }
 `;
 
--- src/data/products.ts
+++ src/data/products.ts
@@ -1,15 +1,154 @@
-export const categories = [
-  {
-    SMART_LIGHTING: {
-      items: [],
-      image: "/images/product_category/smart_lighting.png",
-    },
-  },
-];
+// export const categories = [
+//   {
+//     SMART_LIGHTING: {
+//       items: [],
+//       image: "/images/product_category/smart_lighting.png",
+//     },
+//   },
+// ];
 
 export const products = {
   smart_home_automation_lighting: {
-    items: [],
+    items: [
+      {
+        title: "Tuya Solution 1 Gang Smart Switch",
+        image: "/images/product_category/smart_lighting/product_1.png",
+        description: `
+            Product Category: Light Switch<br/>
+            
+            Product Color: Black, White<br/>
+            
+            Function Description: Remotely Control the Light by APP<br/>
+            
+            Product Size: 86*86*35 mm<br/>
+            
+            Wiring Configuration: Neutral Required at switch point<br/>
+            
+            Switch Type: Touch`,
+      },
+      {
+        title: "2 Gang Smart Switch",
+        image: "/images/product_category/smart_lighting/product_2.png",
+        description: `
+        Product Category: Light Switch<br/>
+
+        Product Color: Black, White<br/>
+        
+        Function Description: Remotely Control the Light by APP<br/>
+        
+        Product Size: 86*86*35 mm<br/>
+        
+        Switch Type: Touch<br/>
+        
+        Wiring Configuration: Neutral Required at switch point<br/>`,
+      },
+      {
+        title: "Orvibo Mixswitch (1-3 Gang)",
+        image: "/images/product_category/smart_lighting/product_3.png",
+        description: `
+        Product type: White, Grey<br/>
+
+        Product Size: 86×86×40mm<br/>
+
+        Power supply: Neutral wire Input voltage: 220V,50/60Hz<br/>
+
+        Load range: 200W/loop<br/>
+
+        Wireless protocol: Zigbee Operating temperature: -20°C~60°C<br/>
+
+        Wireless communication distance: 80m<br/>`,
+      },
+      {
+        title: "MixPad Mini Super Smart Panel",
+        image: "/images/product_category/smart_lighting/product_4.png",
+        description: `
+        Display: 4 inches, 480 480P resolution<br/>
+    
+        Touch layer: multi-touch capacitive screen<br/>
+
+        Speaker: AAC 1813 speaker<br/>
+
+        Microphone: screen brightness adapts to ambient light<br/>
+
+        Power supply: 220V zero live wire power supply Number of scenarios supported; unlimited<br/>
+
+        Support network standards: Wi-Fi, Bluetooth<br/>`,
+      },
+      {
+        title: "MixPad S All-in-one Gateway Panel",
+        image: "/images/product_category/smart_lighting/product_5.png",
+        description: `
+        Storage combination: 2GB RAM 8GB ROM<br/>
+        Display: 4 inches, 480 480P resolution Touch layer: multi-touch capacitive screen.<br/>
+        Microphone: dual microphone array, symmetrical noise reduction.<br/>
+        Light sensor: Screen brightness adapts to ambient light<br/>
+        Distance sensor: automatic wake-up, 10-15cm gesture sensing<br/>
+        Power supply: 220V zero fire wire power supply<br/>
+        Number of supported devices: 350 Support network standards: Wi-Fi, Bluetooth, zigbee3.0<br/>
+        Zigbee networking: built-in gateway<br/>
+        6月25日 210<br/>`,
+      },
+      {
+        title: "MixPad Genie",
+        image: "/images/product_category/smart_lighting/product_6.png",
+        description: `
+        Screen: 3.27 inches' color high-definition touch<br/>
+        Screen Functions: switch lights, home appliance control,<br/>
+        Temperature/date time display Interactive mode: voice, touch screen, buttons, APP<br/>
+        Mobile phone charging: support (Type-C) Wireless protocol: Bluetooth Mesh, Wi-Fi, infrared<br/>
+        Power supply mode: zero live wire power supply, two-end socket power supply, data line power supply<br/>`,
+      },
+    ],
+    image: "/images/product_category/smart_lighting.png",
+  },
+  door_bells_and_access_control: {
+    items: [
+      {
+        title: "",
+        image: "",
+        description: "",
+      },
+    ],
+    image: "/images/product_category/smart_lighting.png",
+  },
+  alexa_voice_assistant: {
+    items: [
+      {
+        title: "",
+        image: "",
+        description: "",
+      },
+    ],
+    image: "/images/product_category/smart_lighting.png",
+  },
+  smart_locks: {
+    items: [
+      {
+        title: "",
+        image: "",
+        description: "",
+      },
+    ],
+    image: "/images/product_category/smart_lighting.png",
+  },
+  smart_detectors: {
+    items: [
+      {
+        title: "",
+        image: "",
+        description: "",
+      },
+    ],
+    image: "/images/product_category/smart_lighting.png",
+  },
+  camera_list_and_specifications: {
+    items: [
+      {
+        title: "",
+        image: "",
+        description: "",
+      },
+    ],
     image: "/images/product_category/smart_lighting.png",
   },
 };
