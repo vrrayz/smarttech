401d62c9871cae69eb95a24acc6365d63193e7b0
Victor <emmyvic98@gmail.com>
2024-01-17 22:37:43 +0100

    boles enterprise

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Footer } from '@/components/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Smart Tech',
+  title: 'BOLES ENTERPRISE',
   description: 'Generated For IOT',
 }
 
